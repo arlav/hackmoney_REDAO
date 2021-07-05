66758ec4c5e48d302c7e101928ea724c3e7078b9
Hico <hicoleer@gmail.com>
2021-07-05 15:41:18 +0200

    NFT minting working

--- ReDaoDapp-nextjs/contracts/RealEstate.sol
+++ ReDaoDapp-nextjs/contracts/RealEstate.sol
@@ -6,6 +6,12 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.2/contracts/token/ERC721/ERC721.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.2/contracts/security/Pausable.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.2/contracts/utils/Counters.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.2/contracts/access/AccessControl.sol";
+
+
 contract RealEstate is ERC721, Pausable, AccessControl {
     using Counters for Counters.Counter;
 
@@ -13,18 +19,33 @@ contract RealEstate is ERC721, Pausable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
+    mapping (uint256 => string) public _tokenURIs;
+
     constructor() ERC721("RealEstate", "RET") {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(PAUSER_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
     }
-
-    function safeMint(address to) public {
+    function mint(address _to, uint256 _tokenId, string memory tokenURI_) external {
         require(hasRole(MINTER_ROLE, msg.sender), 'You do not have minting privileges');
-        _safeMint(to, _tokenIdCounter.current());
-        _tokenIdCounter.increment();
+            _mint(_to, _tokenId);
+            _setTokenURI(_tokenId, tokenURI_);
+    }
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
     }
 
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+        
+        // Concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+    }
     function pause() public {
         require(hasRole(PAUSER_ROLE, msg.sender));
         _pause();
@@ -56,3 +77,18 @@ contract RealEstate is ERC721, Pausable, AccessControl {
         return super.supportsInterface(interfaceId);
     }
 }
+
+
+/*
+IPFS CID's
+Property 1 : Qmbar4ydc8YR1CF8QQmR2XiXdYxT3X9dcncqBkX5VHNxKH
+Property 2 : QmPigmJHmHs1QfKbvk1drsNCjyfZf3hbMLjMwH9JLr7aLL
+Property 3 : QmcRg7hvdxD5UQFsncz2conCmcfeiJWeCCEWpvxM4rzNG1
+bytes32 : 0x0000000000000000000000000000000000000000000000000000000000000000
+
+address : 0xBb604479B2151f3C72a7456873Fdb90f81B676C3
+
+let nft = await RealEstate.deployed()
+await nft.mint('0xBb604479B2151f3C72a7456873Fdb90f81B676C3', 1, 'Qmbar4ydc8YR1CF8QQmR2XiXdYxT3X9dcncqBkX5VHNxKH')
+nft.tokenURI(1)
+*/
--- ReDaoDapp-nextjs/scripts/set-token-uri.js
+++ ReDaoDapp-nextjs/scripts/set-token-uri.js
@@ -3,9 +3,9 @@ const TOKENID = 0
 module.exports = async callback => {
     const re = await RealEstate.deployed()
     console.log('Let\'s set the tokenURI of your properties')
-    const tx = await re.setTokenURI(0, "https://ipfs.io/ipfs/QmaSED9ZSbdGts5UZqueFJjrJ4oHH3GnmGJdSDrkzpYqRS?filename=property1.json")
-    const tx1 = await re.setTokenURI(1, "https://ipfs.io/ipfs/QmTvsVaaHTuMNmwXgbfgkrztFEazAPyzmrb4VSS2PbqLjA?filename=property2.json")
-    const tx2 = await re.setTokenURI(2, "https://ipfs.io/ipfs/QmPZQhiBB6pwcxRgwZe2mx6ZizCPYgq8i4FBMETyWK1V2z?filename=property3.json")
+    const tx = await re.setTokenURI(0, "https://ipfs.io/ipfs/Qmbar4ydc8YR1CF8QQmR2XiXdYxT3X9dcncqBkX5VHNxKH")
+    const tx1 = await re.setTokenURI(1, "https://ipfs.io/ipfs/QmPigmJHmHs1QfKbvk1drsNCjyfZf3hbMLjMwH9JLr7aLL")
+    const tx2 = await re.setTokenURI(2, "https://ipfs.io/ipfs/QmcRg7hvdxD5UQFsncz2conCmcfeiJWeCCEWpvxM4rzNG1")
     console.log(tx)
     callback(tx.tx)
 }
